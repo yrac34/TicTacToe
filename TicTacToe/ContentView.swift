4669ee34bf469acd6193d9e698472716203c5fd3
yrac34 <amyrimchun@gmail.com>
2022-09-02 17:56:04 -0700

    Initial Commit

---
+++ TicTacToe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by Amy Chun on 9/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Amy Chun on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
